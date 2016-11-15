850092e8d455f5172fb67fe55d6be3cb1e1b97a2
Foothz <rickvandenberg92@gmail.com>
2016-11-15 12:48:07 +0100

    Rick doet icoontjes
    Hier zijn de icoontjes oh wat zijn ze mooi.

--- src/bagtrack/Main.java
+++ src/bagtrack/Main.java
@@ -5,7 +5,8 @@ import javafx.application.Application;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.scene.Scene;
-import javafx.scene.input.KeyCombination;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.GridPane;
 import javafx.scene.layout.VBox;
@@ -14,44 +15,49 @@ import javafx.stage.Stage;
 /**
  * @author Jasper & Jimmy
  */
-public class Main extends Application
-{
+public class Main extends Application {
+
     static BorderPane scherm = new BorderPane();
     static VBox menu = new VBox();
-    
+
     //wissel scherm.
-    public static void change(GridPane gridpane){
+    public static void change(GridPane gridpane) {
         scherm.setCenter(gridpane);
     }
-    
-    public static void menu(){
+
+    public static void menu() {
         scherm.setLeft(menu);
     }
-    
+
     @Override
-    public void start(Stage primaryStage)
-    {        
+    public void start(Stage primaryStage) {
         GridPane zoekScherm = Zoekscherm.returnScherm();
         GridPane helpScherm = Helpscherm.returnScherm();
         GridPane inlogScherm = Loginscherm.returnScherm();
         GridPane formulierScherm = Invoerscherm.returnScherm();
         GridPane welkomScherm = Welkomscherm.returnScherm();
         GridPane statistieken = Statistiekenscherm.returnScherm();
+
+        Image Zoom = new Image("zoom_icon&48.png");
+        Image Formulier = new Image("doc_edit_icon&48.png");
+        Image Help = new Image("info_icon&48.png");
+        Image Statistieken = new Image ("chart_line_icon&48.png");
         
         Button uitlogButton = new Button();
         uitlogButton.setText("Uitloggen");
         uitlogButton.setPrefSize(200, 112.5);
         uitlogButton.setOnAction(new EventHandler<ActionEvent>() {
-            @Override 
+            @Override
             public void handle(ActionEvent event) {
                 //Veranderd het huidige scherm naar het inlogscherm en maakt het menu onzichtbaar.
                 scherm.setCenter(inlogScherm);
                 scherm.setLeft(null);
             }
         });
-        
+
         Button statistiekenButton = new Button();
         statistiekenButton.setText("Statistieken");
+        statistiekenButton.setGraphic(new ImageView(Statistieken));
         statistiekenButton.setPrefSize(200, 112.5);
         statistiekenButton.setOnAction(new EventHandler<ActionEvent>() {
             @Override
@@ -59,55 +65,58 @@ public class Main extends Application
                 scherm.setCenter(statistieken);
             }
         });
-                
+
+        
         
         Button zoekButton = new Button();
-        zoekButton.setText("Zoeken");
+        //zoekButton.setText("Zoeken");
+        zoekButton.setGraphic(new ImageView(Zoom));
         zoekButton.setPrefSize(200, 112.5);
         zoekButton.setOnAction(new EventHandler<ActionEvent>() {
-            @Override 
+            @Override
             public void handle(ActionEvent event) {
                 scherm.setCenter(zoekScherm);
             }
         });
-                    
+
         Button helpButton = new Button();
         helpButton.setText("Help");
+        helpButton.setGraphic(new ImageView(Help));
         helpButton.setPrefSize(200, 112.5);
         helpButton.setOnAction(new EventHandler<ActionEvent>() {
-            @Override 
+            @Override
             public void handle(ActionEvent event) {
                 scherm.setCenter(helpScherm);
             }
         });
-        
+
         Button formulierButton = new Button();
         formulierButton.setText("Formulier");
+        formulierButton.setGraphic(new ImageView(Formulier));
         formulierButton.setPrefSize(200, 112.5);
         formulierButton.setOnAction(new EventHandler<ActionEvent>() {
-            @Override 
+            @Override
             public void handle(ActionEvent event) {
                 scherm.setCenter(formulierScherm);
             }
         });
-        
-        menu.getChildren().addAll(zoekButton,formulierButton,helpButton,uitlogButton, statistiekenButton);
-        
+
+        menu.getChildren().addAll(zoekButton, formulierButton, helpButton, uitlogButton, statistiekenButton);
+
         scherm.setCenter(inlogScherm);
-        
+
         Scene scene = new Scene(scherm, 1920, 1080);
         String css = Main.class.getResource("Theme.css").toExternalForm();
         scene.getStylesheets().add(css);
-                
+
         primaryStage.setTitle("Main Screen");
         primaryStage.setScene(scene);
         primaryStage.show();
-       
+
         primaryStage.setFullScreen(true);
     }
-    
-    public static void main(String[] args)
-    {
+
+    public static void main(String[] args) {
         launch(args);
-    }    
+    }
 }
