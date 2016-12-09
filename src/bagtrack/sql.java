7ea2076806f783a4e9d835c7b9bc21b9613b9986
Jimmy Egberts <jimmyegberts@hotmail.com>
2016-12-09 07:10:54 +0100

    sql fix
    woeps

--- src/bagtrack/sql.java
+++ src/bagtrack/sql.java
@@ -66,8 +66,8 @@ public class sql {
         try{
             String driver = "com.mysql.jdbc.Driver";
             String url = "jdbc:mysql://localhost:3306/bagtrack";
-            String username = "root";
-            String password = "EgbertsJimmy003!";
+            String username = "java";
+            String password = "password"; 
             Class.forName(driver); Connection conn = DriverManager.getConnection(url,username,password);
             System.out.println("Connected");
             return conn;
